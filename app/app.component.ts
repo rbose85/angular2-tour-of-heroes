944c4ac8bda6ab90b4819ff269bf2eb91f9b9efe
Rajiv Bose <rbose85@users.noreply.github.com>
2016-09-22 07:54:48 +0100

    two way data binding - using FormsModule

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,11 +8,14 @@ export class Hero {
 @Component({
     selector: 'my-app',
     template: `
-    <h1>{{ title }}</h1>
-    <h2>{{ hero.name }} details!</h2>
-    <div><label>id: </label>{{ hero.id }}</div>
-    <div><label>name: </label>{{ hero.name }}</div>
-`
+        <h1>{{ title }}</h1>
+        <h2>{{ hero.name }} details!</h2>
+        <div><label>id: </label>{{ hero.id }}</div>
+        <div>
+            <label>name: </label>
+            <input type="text" [(ngModel)]="hero.name" placeholder="name">
+        </div>
+    `
 })
 export class AppComponent {
     title = 'Tour of Heroes'
