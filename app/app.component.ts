a1315fb63dd124c15fcb5fd2733d76ed482b9219
Rajiv Bose <rbose85@users.noreply.github.com>
2016-09-23 08:14:13 +0100

    apply 2-space formatting

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core'
 
 export class Hero {
-    id: number
-    name: string
+  id: number
+  name: string
 }
 
 @Component({
-    selector: 'my-app',
-    template: `
-        <h1>{{ title }}</h1>
-        <h2>{{ hero.name }} details!</h2>
-        <div><label>id: </label>{{ hero.id }}</div>
-        <div>
-            <label>name: </label>
-            <input type="text" [(ngModel)]="hero.name" placeholder="name">
-        </div>
+  selector: 'my-app',
+  template: `
+      <h1>{{ title }}</h1>
+      <h2>{{ hero.name }} details!</h2>
+      <div>
+        <label>id:</label>
+        {{ hero.id }}
+      </div>
+      <div>
+        <label>name:</label>
+        <input type="text" [(ngModel)]="hero.name" placeholder="name">
+      </div>
     `
 })
 export class AppComponent {
-    title = 'Tour of Heroes'
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    }
+  title = 'Tour of Heroes'
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 }
