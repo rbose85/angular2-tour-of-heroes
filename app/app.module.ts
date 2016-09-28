f07d03ef616a0a3190fcdfc5be86a115fb6c1bf0
Rajiv Bose <rbose85@users.noreply.github.com>
2016-09-28 08:22:02 +0100

    replace mocked data with fake http service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api'
+import { InMemoryDataService } from './in-memory-data.service'
+
 import { AppComponent } from './app.component'
 import { DashboardComponent } from './dashboard.component'
 import { HeroesComponent } from './heroes.component'
@@ -9,12 +14,18 @@ import { HeroService } from './hero.service'
 import { routing } from './app.routing'
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, routing ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    routing
+  ],
   declarations: [
     AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent
   ],
-  bootstrap: [ AppComponent ],
-  providers: [ HeroService ]
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
 export class AppModule {
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core'
+import { Headers, Http } from '@angular/http'
+
+import 'rxjs/add/operator/toPromise'
+
 import { Hero } from './hero'
-import { HEROES } from './mock-heroes'
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES)
+  private heroesUrl = 'app/heroes'
+
+  constructor(private http: Http) {
   }
 
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000))
-        .then(() => this.getHeroes())
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+        .toPromise()
+        .then(resp => resp.json().data as Hero[])
+        .catch(this.handleError)
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error(`An error occurred: ${error}`)
+    return Promise.reject(error.message || error)
   }
 
   getHero(id: number): Promise<Hero> {
